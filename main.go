0b8c2a6f3b466e8bc37a9657ea15648ac97c2e20
Reuven V. Gonzales <reuven@virtru.com>
2014-10-17 00:58:17 -0400

    Forgot to checkin the main.go changes
    This is part of the initial working beta

--- main.go
+++ main.go
@@ -1,137 +1,84 @@
 package main
 
 import (
-	"code.google.com/p/go.net/websocket"
-	"encoding/json"
 	"fmt"
-	"io"
+	"github.com/ravenac95/termpass/onepass"
 	"os"
+	"strconv"
 	"time"
 )
 
-const HELLO_COMMAND = `
-{
-	"action":"hello",
-	"number":2,
-	"version":"4",
-	"bundleId":"com.googlecode.iterm2",
-	"payload": {
-		"version":"0.0.1",
-		"capabilities": []
+const DEFAULT_TIMEOUT_STRING_SECONDS = "30"
+const DEFAULT_HOST = "termpass://local"
+const WEBSOCKET_URI = "ws://127.0.0.1:6263/4"
+const WEBSOCKET_PROTOCOL = ""
+const WEBSOCKET_ORIGIN = "resource://onepassword4-at-agilebits-dot-com"
+
+func LoadConfiguration() *onepass.Configuration {
+	defaultHost := os.Getenv("TERMPASS_DEFAULT_HOST")
+	if defaultHost == "" {
+		defaultHost = DEFAULT_HOST
+	}
+	return &onepass.Configuration{
+		WebsocketUri:      WEBSOCKET_URI,
+		WebsocketProtocol: WEBSOCKET_PROTOCOL,
+		WebsocketOrigin:   WEBSOCKET_ORIGIN,
+		DefaultHost:       defaultHost,
 	}
 }
-`
 
-type Command struct {
-	Action   string `json:"action"`
-	Number   int    `json:"number"`
-	Version  string `json:"version"`
-	BundleId string `json:"bundleId"`
-}
+func RunTermpass(configuration *onepass.Configuration, done chan bool) {
+	// Load configuration from a file
+	client, err := onepass.NewClientWithConfig(configuration)
 
-type HelloPayload struct {
-	Version      string   `json:"version"`
-	Capabilities []string `json:"capabilities"`
-}
+	if err != nil {
+		os.Exit(1)
+	}
 
-type ShowPopupPayload struct {
-	Url     string            `json:"url"`
-	Options map[string]string `json:"options"`
-}
+	response, err := client.SendHelloCommand()
 
-type HelloCommand struct {
-	Command
-	Payload HelloPayload `json:"payload"`
-}
+	if err != nil {
+		os.Exit(1)
+	}
 
-type ShowPopupCommand struct {
-	Command
-	Payload ShowPopupPayload `json:"payload"`
+	response, err = client.SendShowPopupCommand()
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	password, err := response.GetPassword()
+	fmt.Println(password)
+
+	done <- true
 }
 
 func main() {
-	x := make([]string, 0)
-
-	helloMsg := &HelloCommand{Command{"hello", 0, "4", "com.googlecode.iterm2"}, HelloPayload{"0.0.1", x}}
-
-	helloJson, err := json.Marshal(helloMsg)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(helloJson))
-
-	conn, err := websocket.Dial("ws://127.0.0.1:6263/4", "", "resource://onepassword4-at-agilebits-dot-com")
-
-	if err != nil {
-		fmt.Println("Fatal Error ", err.Error())
-		os.Exit(1)
-	}
-
-	err = websocket.Message.Send(conn, helloJson)
-	if err != nil {
-		fmt.Println("Message failed to send")
-		os.Exit(1)
-	}
-
-	var msg string
-
-	err = websocket.Message.Receive(conn, &msg)
-	if err != nil {
-		fmt.Println("Failed to receive")
-		os.Exit(1)
-	}
-	fmt.Println(msg)
-
-	showPopupMsg := ShowPopupCommand{
-		Command{"showPopup", 1, "4", "com.google.iterm2"},
-		ShowPopupPayload{"https://somewheressh.dev", map[string]string{"source": "toolbar-button"}},
-	}
-
-	showPopupJson, err := json.Marshal(showPopupMsg)
-
-	fmt.Println(string(showPopupJson))
-
-	err = websocket.Message.Send(conn, showPopupJson)
-	if err != nil {
-		fmt.Println("Failed to send")
-		os.Exit(1)
-	}
-
-	err = websocket.Message.Receive(conn, &msg)
-	if err != nil {
-		if err == io.EOF {
-			// graceful shutdown by server
-			return
-		}
-		fmt.Println("Failed to receive anything")
-		os.Exit(1)
-	}
-	fmt.Println(msg)
-
-	//tooLate := make(chan bool)
 	done := make(chan bool)
 
-	go func() {
-		err := websocket.Message.Receive(conn, &msg)
-		if err != nil {
-			if err == io.EOF {
-				// graceful shutdown by server
-				done <- true
-			}
-			fmt.Println("Failed to receive anything")
-			os.Exit(1)
-		}
-	}()
+	configuration := LoadConfiguration()
 
-	fmt.Println("hello there it is")
+	timeoutString := os.Getenv("TERMPASS_TIMEOUT_SECS")
+	if timeoutString == "" {
+		timeoutString = DEFAULT_TIMEOUT_STRING_SECONDS
+	}
 
+	timeout, err := strconv.ParseInt(timeoutString, 10, 16)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	go RunTermpass(configuration, done)
+
+	// Timeout if necessary
 	select {
 	case <-done:
-		fmt.Println("I'm done?!")
-	case <-time.After(10 * time.Second):
-		fmt.Println("too late")
+		// Do nothing no need
+	case <-time.After(time.Duration(timeout) * time.Second):
 		close(done)
+		os.Exit(1)
 	}
+	// Close the app neatly
+	os.Exit(0)
 }
