c2aa3d0d417f83ff4c84172c5f54a23824240e52
Shane Argo <shane@alltheducks.com>
2015-10-13 00:14:33 +1000

    Added support for the 1Password "Password" item type.

--- onepass/response.go
+++ onepass/response.go
@@ -12,23 +12,91 @@ type Response struct {
 	Payload ResponsePayload `json:"payload"`
 }
 
+func (response *Response) GetPassword() (string, error) {
+	if response.Action != "fillItem" {
+		errorMsg := fmt.Sprintf("Response action \"%s\" does not have a password", response.Action)
+		return "", errors.New(errorMsg)
+	}
+
+	itemBytes := []byte(*response.Payload.Item)
+	var item Item
+
+	switch response.Payload.Action {
+		case "fillLogin":
+			var loginItem LoginItem
+			if err := json.Unmarshal(itemBytes, &loginItem); err != nil {
+				return "", err
+			}
+			item = loginItem
+
+		case "fillPassword":
+			var passwordItem PasswordItem
+			if err := json.Unmarshal(itemBytes, &passwordItem); err != nil {
+				return "", err
+			}
+			item = passwordItem
+
+		default:
+			errorMsg := fmt.Sprintf("Payload action \"%s\" does not have a password", response.Payload.Action)
+			return "", errors.New(errorMsg)
+	}
+
+	return item.GetPassword()
+}
+
 type ResponsePayload struct {
-	Item          ItemResponsePayload    `json:"item"`
+	Item          *json.RawMessage       `json:"item"`
 	Options       map[string]interface{} `json:"options"`
 	OpenInTabMode string                 `json:"openInTabMode"`
+	Action  string                       `json:"action"`
 }
 
-type ItemResponsePayload struct {
-	Uuid           string                 `json:"uuid"`
-	NakedDomains   []string               `json:"nakedDomains"`
-	Overview       map[string]interface{} `json:"overview"`
-	SecureContents SecureContents         `json:"secureContents"`
+
+type Item interface {
+	GetPassword() (string, error)
 }
 
-type SecureContents struct {
+
+
+type LoginItem struct {
+	Uuid           string                  `json:"uuid"`
+	NakedDomains   []string                `json:"nakedDomains"`
+	Overview       map[string]interface{}  `json:"overview"`
+	SecureContents LoginItemSecureContents `json:"secureContents"`
+}
+
+func (item LoginItem) GetPassword() (string, error) {
+	for _, field_obj := range item.SecureContents.Fields {
+		if field_obj["designation"] == "password" {
+			return field_obj["value"], nil
+		}
+	}
+
+	return "", errors.New("No password found in the item.")
+}
+
+type LoginItemSecureContents struct {
 	HtmlForm map[string]interface{} `json:"htmlForm"`
 	Fields   []map[string]string    `json:"fields"`
 }
+
+
+
+type PasswordItem struct {
+	Uuid           string                  `json:"uuid"`
+	Overview       map[string]interface{}  `json:"overview"`
+	SecureContents PasswordItemSecureContents `json:"secureContents"`
+}
+
+func (item PasswordItem) GetPassword() (string, error) {
+	return item.SecureContents.Password, nil
+}
+
+type PasswordItemSecureContents struct {
+	Password string `json:"password"`
+}
+
+
 
 func LoadResponse(rawResponseStr string) (*Response, error) {
 	rawResponseBytes := []byte(rawResponseStr)
@@ -41,34 +109,4 @@ func LoadResponse(rawResponseStr string) (*Response, error) {
 	return &response, nil
 }
 
-func (response *Response) GetPassword() (string, error) {
-	if response.Action != "fillItem" {
-		errorMsg := fmt.Sprintf("Response action \"%s\" does not have a password", response.Action)
-		return "", errors.New(errorMsg)
-	}
 
-	for _, field_obj := range response.Payload.Item.SecureContents.Fields {
-		if field_obj["designation"] == "password" {
-			return field_obj["value"], nil
-		}
-	}
-
-	return "", errors.New("No password found in the response")
-}
-
-func getPasswordFromResponse(rawResponseStr string) (string, error) {
-	rawResponseBytes := []byte(rawResponseStr)
-	var response Response
-
-	if err := json.Unmarshal(rawResponseBytes, &response); err != nil {
-		return "", err
-	}
-
-	for _, field_obj := range response.Payload.Item.SecureContents.Fields {
-		if field_obj["designation"] == "password" {
-			return field_obj["value"], nil
-		}
-	}
-
-	return "", errors.New("No password found in the response")
-}
